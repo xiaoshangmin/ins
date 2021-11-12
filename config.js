fc7068e1d60f43e0f0d2c3ab9e41a6f5ec85b293
xiaoshangmin <Xsmxsm0220@gmail.com>
2021-11-12 11:34:43 +0800

    修改名称

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let local_host = "http://xsm.club.com/api/v1";
 let alpha_host = "https://api.wowyou.cc/api/v1";
 let host = 'https://api.wowyou.cc/api/v1';
-let qiniu_host = 'http://cdn.wowyou.cc';
+let qiniu_host = 'https://cdn.wowyou.cc';
 const accountInfo = wx.getAccountInfoSync();
 if (accountInfo.miniProgram.envVersion == "develop") {
   host = alpha_host;
