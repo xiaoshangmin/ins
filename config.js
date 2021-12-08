cd13d24095b63f213c050a14983ea3e3094cb290
xiaoshangmin <Xsmxsm0220@gmail.com>
2021-12-08 17:43:40 +0800

    u

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
-let serve_id = 'a87ff679a2f3e71d9181a67b7542122c';
 let app_name = 'emoji';
+let serve_id = 'e4da3b7fbbce2345d7772b0674a318d5';
+let aplha_serve_id = 'a87ff679a2f3e71d9181a67b7542122c';
 let localhost = 'http://xsm.saas.com';
 let alpha_host = 'https://saas.wowyou.cc';
+let stable_host = 'https://saas.wowyou.cc';
 let cdn_host = 'https://cdn.wowyou.cc';
 let base_url = `${localhost}/${app_name}/v1`;
 let auth_url = `${localhost}/api/v1`;
@@ -9,6 +11,7 @@ const accountInfo = wx.getAccountInfoSync();
 if (accountInfo.miniProgram.envVersion != "develop") {
   base_url = `${alpha_host}/${app_name}/v1`;
   auth_url = `${alpha_host}/api/v1`;
+  serve_id = aplha_serve_id
 }
 console.log(base_url,accountInfo)
 let config = {
